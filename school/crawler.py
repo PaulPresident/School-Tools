13c046f0640929b05142f0d2c878613d6cba9bb5
PaulPresident <47800285+PaulPresident@users.noreply.github.com>
2021-01-12 17:40:11 -0500

    Jan 12

--- school/crawler.py
+++ school/crawler.py
@@ -1,15 +1,21 @@
 import scrapy
+from scrapy.http import FormRequest
 
 class crawler(scrapy.Spider):
-    name = 'name'
+    name = 'login'
     start_urls = ['https://pamt-sapphire.k12system.com/CommunityWebPortal/Welcome.cfm']
 
     def parse(self, response):
-        scrapy.FormRequest.from_response(
-            response,
-            formdata={'j_username': '2022sedrapo', 'j_password': '1016335', 'j_pin': '36415'}
-            # callback=self.after_login
-        )
+        csrf_token = 'BE9A2E8ABEA6650585E801319060B1025753D5D5'
+        yield FormRequest.from_response(response, formdata={'csrf_token': csrf_token, 'j_username': '2022sedrapo', 'j_password': '1016335', 'j_pin': '36415'}, callback=self.parse_after_login)
 
-        grade = response.css('ul.StudentList li a.student::text').getall()
-        yield {'grade': grade}
+    def parse_after_login(self, response):
+        # this most likely did not send me to the link idk tho
+        # scrapy.Request(url='https://pamt-sapphire.k12system.com/CommunityWebPortal/Backpack/StudentClasses.cfm?STUDENT_RID=1106151')
+        cum = str(response.body)
+        print(cum) # prints the body
+        cum = re.sub('\\n', ' ', cum)
+        # i still do not know how to scrape fuck
+        grade = response.xpath('/html/body/div[2]/table/tbody/tr[3]/td[2]/table/tbody/tr/td[2]/div[3]/table/tbody/tr[3]/td[1]').getall()
+        print(grade)
+        yield grade
